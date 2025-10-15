52d11b6feb5e6cd3672d5e2591c8f8013e0c53d8
ankurraj007 <rajguptaankur@gmail.com>
2025-10-15 16:55:17 +0530

    db connect

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import express from "express"
+import connectDB from "./db/db.js"
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './env' })
+
+connectDB()
+
+
+
+
+
+
+//    Way 1 of connecting DB
+//     /*()() is called IIFE — Immediately Invoked Function Expression.The outer parentheses define an anonymous function.The first () invokes it.That function returns another function.The second () invokes the returned function.*/
+
+//     /* In async function, await pauses the execution unless promise from async is fulfilled. if promise rejected, it will throw error. */
+
+//     (async () => {
+//         try {
+//             mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//             app.on("error", (error) => {
+//                 console.log("Error:", error)
+//                 throw error
+//             })
+//             app.listen(process.env.PORT, () => {
+//                 console.log(`App is listening on PORT ${process.env.PORT}`)
+//             })
+//         }
+//         catch (error) {
+//             console.error("ERROR", error)
+//             throw error
+//         }
+//     })() 
