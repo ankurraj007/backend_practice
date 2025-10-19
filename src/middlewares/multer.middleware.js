aabeea374e4068e4e4b867fde084d6f500ee1697
ankurraj007 <rajguptaankur@gmail.com>
2025-10-19 14:21:23 +0530

    Set up Multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer"
+
+/*Multer is a middleware for parsing and storing uploaded files from HTTP requests.Handles file transfer from client → server*/
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+export const upload = multer({ storage: storage })
